d52699490325685facf4e926c687a0923af4b09f
belungas <nabilhairie00@gmail.com>
2022-03-18 17:46:43 +0800

    ryumd

---
+++ ryudata/allmenu.js
@@ -0,0 +1,206 @@
+exports.allmenu = (prefix) => {
+	return`Media Sosial ❄️
+Instagram : belu_ngas ._
+
+*👤 OWNER MENU*
+⚘❄️ ${prefix}chat [option]
+⚘❄️ ${prefix}join [link]
+⚘❄️ ${prefix}leave
+⚘❄️ ${prefix}self
+⚘❄️ ${prefix}public
+⚘❄️ ${prefix}setppbot
+⚘❄️ ${prefix}block @user
+⚘❄️ ${prefix}unblock @user
+⚘❄️ ${prefix}bc
+⚘❄️ ${prefix}bcgc
+⚘❄️ ${prefix}setcmd [reply sticker/pesan]
+⚘❄️ ${prefix}delcmd [reply sticker/pesan]
+⚘❄️ ${prefix}listcmd
+⚘❄️ ${prefix}lockcmd
+
+*👥 GROUP MENU*
+⚘❄️ ${prefix}antilink
+⚘❄️ ${prefix}linkgroup
+⚘❄️ ${prefix}ephemeral [option]
+⚘❄️ ${prefix}setppgc
+⚘❄️ ${prefix}setname [text]
+⚘❄️ ${prefix}group [option]
+⚘❄️ ${prefix}add @user
+⚘❄️ ${prefix}kick @user
+⚘❄️ ${prefix}promote @user
+⚘❄️ ${prefix}demote @user
+
+*📦 DOWNLOAD MENU*
+⚘❄️ ${prefix}tiktoknowm [url]
+⚘❄️ ${prefix}tiktokwm [url]
+⚘❄️ ${prefix}tiktokaudio [url]
+⚘❄️ ${prefix}instagram [url]
+⚘❄️ ${prefix}twitter [url]
+⚘❄️ ${prefix}facebook [url]
+⚘❄️ ${prefix}ytmp4 [url]
+⚘❄️ ${prefix}ytmp3 [url]
+⚘❄️ ${prefix}play [judul]
+⚘❄️ ${prefix}ytsearch [judul]
+
+*🔎 SEARCH MENU*
+⚘❄️ ${prefix}pinterest [query]
+⚘❄️ ${prefix}pantun
+⚘❄️ ${prefix}google [query]
+⚘❄️ ${prefix}gimage [query]
+⚘❄️ ${prefix}igstalk [username]
+
+*🗞️ INFORMASI MENU*
+⚘❄️ ${prefix}infogempa
+⚘❄️ ${prefix}faktaunik
+⚘❄️ ${prefix}ssweb
+
+*✍️ NULIS MENU*
+⚘❄️ ${prefix}nulisbiasa
+⚘❄️ ${prefix}nuliskanan
+⚘❄️ ${prefix}nuliskiri
+⚘❄️ ${prefix}foliokanan
+⚘❄️ ${prefix}foliokiri
+
+*👭 ASUPAN MENU*
+⚘❄️ ${prefix}asupanhijaber
+⚘❄️ ${prefix}asupancecan
+⚘❄️ ${prefix}asupanbocil
+⚘❄️ ${prefix}asupanukhty
+⚘❄️ ${prefix}asupanghea
+⚘❄️ ${prefix}asupanrika
+⚘❄️ ${prefix}asupansantuy
+
+*👩 CECAN MENU*
+⚘❄️ ${prefix}china
+⚘❄️ ${prefix}vietnam
+⚘❄️ ${prefix}thailand
+⚘❄️ ${prefix}indonesia
+⚘❄️ ${prefix}korea
+⚘❄️ ${prefix}japan
+⚘❄️ ${prefix}malaysia
+
+*🎮 FUN MENU*
+⚘❄️ ${prefix}truth
+⚘❄️ ${prefix}dare
+⚘❄️ ${prefix}bagaimanakah [teks]
+⚘❄️ ${prefix}kapankah [teks]
+⚘❄️ ${prefix}dimanakah [teks]
+⚘❄️ ${prefix}suit [✌️/✊/✋]
+⚘❄️ ${prefix}aduayam [🐥/🐤🐓/🐣/🦃]
+⚘❄️ ${prefix}suitpvp [tag user]
+
+*🎰 GAME MENU*
+⚘❄️ ${prefix}tebak lagu
+⚘❄️ ${prefix}tebak gambar
+⚘❄️ ${prefix}tebak kata
+⚘❄️ ${prefix}tebak kalimat
+⚘❄️ ${prefix}tebak lirik
+⚘❄️ ${prefix}tebak lontong
+⚘❄️ ${prefix}kuismath
+
+*🎡 RANDOM MENU*
+⚘❄️ ${prefix}darkjokes
+⚘❄️ ${prefix}meme
+⚘❄️ ${prefix}quotes
+⚘❄️ ${prefix}quotes2
+⚘❄️ ${prefix}katabijak
+⚘❄️ ${prefix}wallgaming
+⚘❄️ ${prefix}wallpentol
+⚘❄️ ${prefix}wallcogan
+⚘❄️ ${prefix}wallcecan
+⚘❄️ ${prefix}wallestetik
+⚘❄️ ${prefix}wallislami
+⚘❄️ ${prefix}wallbkackpink
+⚘❄️ ${prefix}wallpaperanime
+
+*⛩️ ANIME MENU*
+⚘❄️ ${prefix}quotesanime
+⚘❄️ ${prefix}wallpaper
+⚘❄️ ${prefix}waifu
+⚘❄️ ${prefix}loli
+⚘❄️ ${prefix}cry
+⚘❄️ ${prefix}kill
+⚘❄️ ${prefix}hug
+⚘❄️ ${prefix}pat
+⚘❄️ ${prefix}lick
+⚘❄️ ${prefix}kiss
+⚘❄️ ${prefix}bite
+⚘❄️ ${prefix}yeet
+⚘❄️ ${prefix}neko
+⚘❄️ ${prefix}bully
+⚘❄️ ${prefix}bonk
+⚘❄️ ${prefix}wink
+⚘❄️ ${prefix}poke
+⚘❄️ ${prefix}nom
+⚘❄️ ${prefix}slap
+⚘❄️ ${prefix}smile
+⚘❄️ ${prefix}wave
+⚘❄️ ${prefix}awoo
+⚘❄️ ${prefix}blush
+⚘❄️ ${prefix}smug
+⚘❄️ ${prefix}glomp
+⚘❄️ ${prefix}happy
+⚘❄️ ${prefix}dance
+⚘❄️ ${prefix}cringe
+⚘❄️ ${prefix}cuddle
+⚘❄️ ${prefix}highfive
+⚘❄️ ${prefix}shinobu
+⚘❄️ ${prefix}megumin
+⚘❄️ ${prefix}handhold
+
+*🔞 NSFW MENU*
+⚘❄️ ${prefix}blowjob
+⚘❄️ ${prefix}hentai
+⚘❄️ ${prefix}yuri
+⚘❄️ ${prefix}neko
+⚘❄️ ${prefix}cum
+
+*🎨 MAKER MENU*
+⚘❄️ ${prefix}toimage
+⚘❄️ ${prefix}smeme
+⚘❄️ ${prefix}smeme2
+⚘❄️ ${prefix}sticker
+⚘❄️ ${prefix}tovideo
+⚘❄️ ${prefix}togif
+⚘❄️ ${prefix}tourl
+⚘❄️ ${prefix}emojimix 😎+😂
+
+*👨‍🎨 TEXTPRO MENU*
+⚘❄️ ${prefix}serti1 [teks]
+⚘❄️ ${prefix}serti2 [teks]
+⚘❄️ ${prefix}serti3 [teks]
+⚘❄️ ${prefix}blackpink [teks]
+⚘❄️ ${prefix}neon [teks]
+⚘❄️ ${prefix}magma [teks]
+⚘❄️ ${prefix}3dstone [teks]
+⚘❄️ ${prefix}halloween [teks]
+⚘❄️ ${prefix}horror [teks]
+⚘❄️ ${prefix}larva [teks]
+⚘❄️ ${prefix}toxic [teks]
+⚘❄️ ${prefix}marvel [teks|teks]
+⚘❄️ ${prefix}potter [teks]
+⚘❄️ ${prefix}pornhub [teks|teks]
+⚘❄️ ${prefix}tahta [teks]
+
+*☕ OTHER MENU*
+⚘❄️ ${prefix}simi [teks]
+⚘❄️ ${prefix}ping
+⚘❄️ ${prefix}owner
+⚘❄️ ${prefix}creator
+⚘❄️ ${prefix}menu / ${prefix}help / ${prefix}?
+⚘❄️ ${prefix}delete
+⚘❄️ ${prefix}sc
+⚘❄️ ${prefix}tqto
+⚘❄️ ${prefix}donasi
+⚘❄️ ${prefix}rules
+⚘❄️ ${prefix}sewabot
+⚘❄️ ${prefix}listgc
+⚘❄️ ${prefix}liston
+⚘❄️ ${prefix}liston
+⚘❄️ ${prefix}report
+⚘❄️ ${prefix}req
+
+Fitur Nya Masih Dikit Jadi
+Subscribe For Update 🤙
+`
+}
---
+++ ryudata/welkam.js
@@ -0,0 +1,25 @@
+//EDIT SESUKA KALIAN 😉
+
+// UCAPAN WELCOME
+exports.welkam = () => {
+	return `Intro Dulu Biar Keren 🤙
+
+╭┈˖⋆ ❁────
+┊- 👻nama :
+┊- 👀umur :
+┊- 🏙️askot :
+╰┄───➤ °♡•.
+
+Semoga Kamu Senang Berada Disini
+Serta Jangan Lupa Untuk Membaca
+Dan Mematuhi Rules Yang Ada
+`
+}
+
+// UCAPAN GOODBYE
+exports.goodbye = () => {
+	return `Tetap Putus Asa Jangan
+Semangat Dan Jadilah Beban
+Keluarga 🤙🗿
+`
+}
